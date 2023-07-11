044b6551240528b81c0abc0d1b6a0322fe0277bc
Albert Stanley <albertjstanley@gmail.com>
2023-07-11 12:51:12 -0700

    start dag visuals

---
+++ visuals/pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactFlow from 'reactflow';
+
+import 'reactflow/dist/style.css';
+
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
+export default function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+    </div>
+  );
+}
