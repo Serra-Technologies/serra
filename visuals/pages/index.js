81acb2690f70dfb73c0d7626c4aeeb922b808f79
Albert Stanley <albertjstanley@gmail.com>
2023-07-11 18:10:24 -0700

    reactflow start

--- visuals/pages/index.js
+++ visuals/pages/index.js
@@ -1,18 +1,42 @@
-import React from 'react';
-import ReactFlow from 'reactflow';
+import React, { useCallback ,useState} from 'react';
+import ReactFlow, {
+  MiniMap,
+  Controls,
+  Background,
+  MarkerType,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+} from 'reactflow';
 
 import 'reactflow/dist/style.css';
 
+
 const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+  { id: '1', position: { x: 500, y: 100 }, data: { label: '1' },type: 'input' },
+  { id: '2', position: { x: 500, y: 200 }, data: { label: '2' } }
 ];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+const initialEdges = [{ id: 'e1-2', 
+                        source: '1', 
+                        target: '2',
+                        markerEnd: {
+                          type: MarkerType.ArrowClosed,
+                        },}];
+
+
 
 export default function App() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+    <div style={{ width: '100vw', height: '100vh', border: "3px"}}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+      />
     </div>
   );
 }
