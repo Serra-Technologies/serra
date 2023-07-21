19b2ee3e5c4c937b63cad68349dedae05adbd692
Albert Stanley <albertjstanley@gmail.com>
2023-07-21 01:58:14 -0700

    remove linear run

--- serra/run.py
+++ serra/run.py
@@ -22,62 +22,17 @@ def create_job_yaml(job_name):
     starter_config = f"name: {job_name}\nsteps: []"
     write_to_file(file_path, starter_config)
 
-
-def run_job_simple_linear(cf: ConfigParser, is_local):
-    """
-    Current assumptions
-    - at least one step
-    - first step is a read
-    - only one read in job steps
-    """
-    steps = cf.get_job_steps()
-
-    reader_step = steps[0]
-    logger.info(f"Executing {reader_step}")
-    reader_class_name = cf.get_class_name_for_step(reader_step)
-    reader_config = cf.get_config_for_step(reader_step)
-
-    full_reader_class_name = convert_name_to_full(reader_class_name)
-    reader_object = import_class(full_reader_class_name)
-
-    df = reader_object(reader_config).read()
-
-    if is_local:
-        df = df.limit(10)
-
-    for step in steps[1:-1]:
-        logger.info(f"Executing {step}")
-        # Get coressponding class
-        class_name = cf.get_class_name_for_step(step)
-        config = cf.get_config_for_step(step)
-
-        full_class_name = convert_name_to_full(class_name)
-        step_object = import_class(full_class_name)
-        df = step_object(config).transform(df)
-
-    # Assume final step is a write
-    writer_step = steps[-1]
-    logger.info(f"Executing {writer_step}")
-    writer_class_name = cf.get_class_name_for_step(writer_step)
-    writer_config = cf.get_config_for_step(writer_step)
-
-    full_writer_class_name = convert_name_to_full(writer_class_name)
-    writer_object = import_class(full_writer_class_name)
-    writer_object(writer_config).write(df)
-
-    # Commenting this out because it doesn't look good with large dfs
-    df.show()
-
 def run_job_from_job_dir(job_name):
     user_configs_folder = get_path_to_user_configs_folder()
     config_path = f"{user_configs_folder}/{job_name}.yml"
     cf = ConfigParser.from_local_config(config_path)
-    run_job_simple_linear(cf, True)
+    # run_job_simple_linear(cf, True)
+    run_job_with_graph(cf)
 
 def run_job_from_aws(job_name):
     try:
         cf = ConfigParser.from_s3_config(f"{job_name}.yml")
-        run_job_simple_linear(cf, False)
+        run_job_with_graph(cf)
     except Exception as e:
         logger.error(e)
         exit(1)
