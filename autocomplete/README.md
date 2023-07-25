f3cd0c46dab3048c11fd9f1e0a5d77d3af59535b
Albert Stanley <albertjstanley@gmail.com>
2023-07-25 11:47:02 -0700

    add new autocomplete

---
+++ autocomplete/README.md
@@ -0,0 +1,38 @@
+# Setup autocomplete for VSCode
+## Step 1: Install redhat.vscode-yaml
+Link: https://marketplace.visualstudio.com/items?itemName=redhat.vscode-yaml
+
+## Step 2: Get the path to the serra_yaml_schema.json.
+
+
+```
+readlink -f ./output/serra_yaml_schema.json
+
+/path/to/serra_yaml_schema.json
+```
+
+## Step 3: Configure VSCode YAML settings
+Inside settings.json (https://code.visualstudio.com/docs/getstarted/settings#_settingsjson):
+```
+    "yaml.schemas": {
+        "file:///path/to/serra_yaml_schema.json": ["*serra.yaml"]
+    }
+```
+
+Now any files createed with the extension ```.serra.yaml``` should show proper autocomplete for the Serra framework.
+
+# Usage
+Hit ctl+space when writing the yaml file to get suggestions.
+
+
+# Modifying autocomplete
+These scripts are set up to be easy to set up autocomplete for Serra config files.
+Inside the inputs directory, you can modify the specs.json file to specify the parameters that each transformer, reader, or writer takes.
+
+##  Generate JSON Schemas
+```python3 gen_schemas.py```
+
+
+## Read more about JSON Schema
+https://json-schema.org/
+
---
+++ autocomplete/gen_schemas.py
@@ -0,0 +1,55 @@
+import json
+
+
+def read_json_file(file_path):
+    # Opening JSON file
+    data = {}
+    with open(file_path) as json_file:
+        data = json.load(json_file)
+
+    return data
+
+def write_to_file(data_dict, output_file):
+    try:
+        with open(output_file, 'w') as file:
+            json.dump(data_dict, file, indent=4, sort_keys=True)
+        print(f"Dictionary has been pretty printed to '{output_file}' successfully.")
+    except Exception as e:
+        print(f"Error occurred while writing to file: {e}")
+
+SPEC_PATH = "./inputs/specs.json"
+SCHEMA_TEMPLATE = "./templates/schema_template.json"
+BLOCK_TEMPLATE = "./templates/transformer_template.json"
+OUTPUT_DIR = "./output"
+
+
+specs = read_json_file(SPEC_PATH)
+schema_template = read_json_file(SCHEMA_TEMPLATE)
+transformer_template = read_json_file("./templates/transformer_template.json")
+
+
+# Create the main schema json
+for class_name in specs:
+    new_property = {
+            "type": "object",
+            "properties": {f"{class_name}" : { "$ref": f"./{class_name}.json" }},
+            "additionalProperties": False
+          }
+
+    schema_template["patternProperties"][".*"]['oneOf'].append(new_property)
+
+# Create the schemas for each class
+for class_name in specs:
+    class_schema = {
+        "type": "object",
+        "properties": {
+            
+        },
+        "additionalProperties": False
+    }
+
+    for property,property_type in specs[class_name].items():
+        class_schema['properties'][property] = {"type": property_type}
+    write_to_file(class_schema, f"{OUTPUT_DIR}/{class_name}.json")
+
+write_to_file(schema_template,f"{OUTPUT_DIR}/serra_yaml_schema.json")
