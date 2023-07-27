e4622af53f0f3578082fdbb8f6e74ef7de5a5f18
Albert Stanley <albertjstanley@gmail.com>
2023-07-27 10:57:14 -0700

    use serra exception

--- serra/tests.py
+++ serra/tests.py
@@ -1,10 +1,11 @@
 from pyspark.sql.functions import when, count, isnull
+from serra.exceptions import SerraRunException
 
 def nulls_test(df):
     has_nulls = df.select([count(when(isnull(c), c)).alias(c) for c in df.columns])
     if has_nulls:
-        raise Exception("Null values were found in the DataFrame")
+        raise SerraRunException("Null values were found in the DataFrame")
     
 def duplicates_test(df):
     if df.count() > df.dropDuplicates(df.columns).count():
-        raise ValueError('Data has duplicates')
+        raise SerraRunException('Data has duplicates')
