8101bd72d1de2c0beda4c5e61f9c02f2933367e5
alanhlwang <alanw23@g.ucla.edu>
2023-07-31 22:39:56 -0700

    Update translate url

--- serra/config.py
+++ serra/config.py
@@ -3,4 +3,4 @@ import os
 PACKAGE_PATH = os.path.dirname(__file__)
 LOCAL_PATH_TO_WHEEL = f"{PACKAGE_PATH}/../dist/serra-0.1-py3-none-any.whl"
 WHEEL_FILE_NAME_IN_BUCKET = "serra-0.1-py3-none-any.whl"
-TRANSLATE_URL="http://127.0.0.1:5000"
+TRANSLATE_URL="https://serra-translate-59cfd3dacac9.herokuapp.com/"
