4bddaccd58f4067faf5b0593f67dc09278235405
Albert Stanley <albertjstanley@gmail.com>
2023-08-03 19:44:00 -0700

    remove unused imports

--- serra/writers/__init__.py
+++ serra/writers/__init__.py
@@ -1,6 +1,5 @@
 from .local_writer import LocalWriter
 from .writer import Writer
-from .s3_writer import S3Writer
 from .databricks_writer import DatabricksWriter
 from .amazon_writer import AmazonS3Writer
 from .snowflake_writer import SnowflakeWriter
