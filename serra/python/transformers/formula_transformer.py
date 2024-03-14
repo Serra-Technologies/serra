562cb2c74d35f722d702459593fe8437d31cdcb0
Albert Stanley <albertjstanley@gmail.com>
2024-03-14 00:20:32 -0700

    add formula tranformer

--- serra/python/transformers/formula_transformer.py
+++ serra/python/transformers/formula_transformer.py
@@ -1,44 +1,34 @@
 from serra.python.base import PythonTransformer
-import math, random, numpy, re
+import pandas as pd
+import numpy as np
 
 class FormulaTransformer(PythonTransformer):
     """
     Allows business users to write their own math transforms
     """
 
-    def __init__(self, text, output_column):
-        self.text = text
-        self.output_column = output_column
+    def __init__(self, new_column_name, if_condition, then_statement, else_statement):
+        self.new_column_name = new_column_name
+        self.if_condition = if_condition
+        self.then_statement = then_statement
+        self.else_statement = else_statement
 
     def transform(self, df):
-        # Input: min(random(facility))
-        # Output: Eval min(random.randint(df['facility']))
-            # Use regex to map business user functions to valid Python functions (if not in map, check if valid and use same one), add df.args
+        def is_valid_eval_output(output):
+            return type(output) == pd.Series or type(output) == bool or output == None
 
-        math_map = {
-            "random": "random.randint",
-            **{func: f"numpy.{func}" for func in ['sqrt', 'exp', 'floor', 'pow', 'ceil', 'fabs']} # Unpack dictionary of math module function mappings
-            # min, max are valid
-        }
 
-        def replace_func(match):
-            # Input: min(sqrt(col1))
-            # Output: min(math.sqrt(col1))
+        # Evaluate expressions and ensure they are of the correct type
+        eval_if_condition = df.eval(self.if_condition)
+        eval_then_condition = df.eval(self.then_statement)
+        eval_else_condition = df.eval(self.else_statement)
+        # assert is_valid_eval_output(eval_if_condition)
+        # assert is_valid_eval_output(eval_then_condition)
+        # assert is_valid_eval_output(eval_else_condition)
 
-            if math_map.get(match.group(1)) is not None: # Group 1: Function, Group 2: (
-                return math_map.get(match.group(1)) + match.group(2)
-            else: # Valid function, todo implement valid function check else eval might fail
-                return match.group(0)
-            
-        
-        valid_function = re.sub(r"(\w+)(\()", replace_func, self.text)
-        valid_function_args = re.sub(r"\(([^()]+)\)", r"(df['\1'])", valid_function)
-
-        df[self.output_column] = eval(valid_function_args)
+        out = np.where(eval_if_condition, eval_then_condition, eval_else_condition)
+        df[self.new_column_name] = out
         return df
-
-
-        
 
 
 
